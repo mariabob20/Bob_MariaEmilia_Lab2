ef79afece35cd3d656beaaf17fb182c8ed2d89bb
Maria Bob <maria_bob16@yahoo.com>
2023-11-05 14:48:38 +0200

    Lab4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,20 @@
-﻿namespace Bob_MariaEmilia_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bob_MariaEmilia_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string AuthorFirstName { get; set; }
         public string AuthorLastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return AuthorFirstName + " " + AuthorLastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
